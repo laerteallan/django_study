ca195268077a7c21dd37339c9655dd5a124bafd2
Laerte Allan de Oliveira Araujo <Laerte.araujo@fs.com.br>
2019-01-02 08:48:15 -0200

    Improve the test

--- test_django/catalog/views.py
+++ test_django/catalog/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
-from django.http import  JsonResponse
+from django.http import JsonResponse
 # Create your views here.
 #
 import json
 
-from django.http import  HttpResponse
+from django.http import HttpResponse
 
 response_data = {}
 response_data['result'] = 'laerte allan'
 response_data['message'] = 'teste'
 
 
-
 def test_view(request):
-
-
-    return JsonResponse(response_data, status=500)
+    return JsonResponse(response_data)
--- test_django/test_django/urls.py
+++ test_django/test_django/urls.py
@@ -18,9 +18,14 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
